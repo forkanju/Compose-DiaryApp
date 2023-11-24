3f6eef3daa199b2fb8c76b524848aac82b0ab2d1
forkanju <forkanju44rth@gmail.com>
2023-11-24 22:26:15 +0600

    mongodb and google cloud authentication setup and generated client id.

--- app/src/main/java/com/example/compose_diaryapp/util/Constants.kt
+++ app/src/main/java/com/example/compose_diaryapp/util/Constants.kt
@@ -2,4 +2,5 @@ package com.example.compose_diaryapp.util
 
 object Constants {
     const val APP_ID = "mydiaryapp-yfxzu"
+    const val CLIENT_ID = "541384118171-cn04rl8vvifs1n0lsoh17r94idloo441.apps.googleusercontent.com"
 }
