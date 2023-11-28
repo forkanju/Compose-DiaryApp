eceaa227d687c423d65af812f264f480eb4ab7f4
forkanju <forkanju44rth@gmail.com>
2023-11-28 23:02:27 +0600

    authentication screen design done

--- app/src/main/java/com/example/compose_diaryapp/navigation/NavGraph.kt
+++ app/src/main/java/com/example/compose_diaryapp/navigation/NavGraph.kt
@@ -1,5 +1,8 @@
 package com.example.compose_diaryapp.navigation
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
@@ -7,8 +10,11 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.example.compose_diaryapp.presentation.screens.auth.AuthenticationScreen
 import com.example.compose_diaryapp.util.Constants.WRITE_SCREEN_ARGUMENT_KEY
 
+@OptIn(ExperimentalMaterial3Api::class)
+@RequiresApi(Build.VERSION_CODES.N)
 @Composable
 fun SetupNavGraph(startDestination: String, navController: NavHostController) {
 
@@ -20,9 +26,15 @@ fun SetupNavGraph(startDestination: String, navController: NavHostController) {
 
 }
 
+@ExperimentalMaterial3Api
 fun NavGraphBuilder.authenticationRoute() {
     composable(route = Screen.Authentication.route) {
+        AuthenticationScreen(
+            loadingState = false,
+            onButtonClicked = {
 
+            }
+        )
     }
 }
 
---
+++ app/src/main/java/com/example/compose_diaryapp/presentation/screens/auth/AuthenticationContent.kt
@@ -0,0 +1,80 @@
+package com.example.compose_diaryapp.presentation.screens.auth
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.compose_diaryapp.R
+import com.example.compose_diaryapp.presentation.components.GoogleButton
+
+@Composable
+fun AuthenticationContent(
+    loadingState: Boolean,
+    onButtonClicked: () -> Unit
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(weight = 9f)
+                .fillMaxWidth()
+                .padding(all = 40.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Column(
+                modifier = Modifier.weight(weight = 10f),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    modifier = Modifier.size(120.dp),
+                    painter = painterResource(id = R.drawable.google_logo),
+                    contentDescription = "Google Logo"
+                )
+                Spacer(modifier = Modifier.height(height = 20.dp))
+                Text(
+                    text = stringResource(id = R.string.auth_title),
+                    fontSize = MaterialTheme.typography.bodyMedium.fontSize
+                )
+                Text(
+                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.38f),
+                    text = stringResource(id = R.string.auth_subtitle),
+                    fontSize = MaterialTheme.typography.bodyMedium.fontSize
+                )
+            }
+            Column(
+                modifier = Modifier.weight(weight = 2f),
+                verticalArrangement = Arrangement.Center
+            ) {
+                GoogleButton(
+                    loadingState = loadingState,
+                    onClick = onButtonClicked
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AuthenticationContentPreview() {
+    AuthenticationContent(loadingState = false) {
+
+    }
+}
